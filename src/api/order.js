78508c95043b6193ec06f90e9a89ba440c6f1c23
jing <985812572@qq.com>
2018-04-16 21:10:49 +0800

    订单详情页

---
+++ src/api/order.js
@@ -0,0 +1,30 @@
+/**
+ * Created by user on 2018/4/8.
+ */
+import axios from 'axios'
+import qs from 'qs'
+
+export function checkOut(entities, geohash) {
+  const url = '/restapi/booking/v1/carts/checkout'
+  const data = {
+    address_id: 0,
+    come_from: 'mobile',
+    deliver_time: '',
+    entities,
+    geohash,
+    hongbao_action: 0,
+    hongbao_sn: '',
+    merchant_coupon_action: 1,
+    merchant_coupon_id: '',
+    paymethod_id: -1,
+    sig: '',
+    sub_channel: '',
+    tying_entities: [],
+    user_id: 1443333514
+  }
+
+  return axios.post(url, qs.stringify(data))
+  //   .then((response) => {
+  //   res.json(response.data)
+  // })
+}
